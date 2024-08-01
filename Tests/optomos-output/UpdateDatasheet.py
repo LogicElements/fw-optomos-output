0fcf0937ee13d496ca7600a87ec08ebe00eb5651
LE\jan.bartovsky <jan.bartovsky@logicelements.cz>
2024-08-01 09:11:57 +0200

    Mod register map. Fix update datasheet.

--- Tests/optomos-output/UpdateDatasheet.py
+++ Tests/optomos-output/UpdateDatasheet.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print(f"=== START === " + datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S ==="))
     err = 0
 
-    docx_update_datasheet("PrecRtdSim_Modbus.json", "../../Documents/PRecRtdSim-datasheet.docx")
+    docx_update_datasheet("OptoOutMod_Modbus.json", "../../Documents/OptoOutMod-datasheet.docx")
 
     print(f"=== END === " + datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S ==="))
     print('=== RESULT === {} === '.format('SUCCESS' if err == 0 else '{} ERRORS'.format(err)))
